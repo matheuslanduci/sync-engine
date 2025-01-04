c853d1374459cbdf5b0af93d7296db3aa0c02b64
matheuslanduci <matheuslanduci@gmail.com>
2025-01-04 11:08:32 -0300

    final version

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,101 @@
+import { db, type Todo } from './db.js'
+
 import { serve } from '@hono/node-server'
 import { createNodeWebSocket } from '@hono/node-ws'
+import { cors } from 'hono/cors'
+import type { WSContext } from 'hono/ws'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
 const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app })
 
+app.use(cors())
+
 app.get('/', (c) => {
 	return c.text('Hello Hono!')
 })
 
+app.post('/sync', async (c) => {
+	const body = await c.req.json()
+
+	const items = body.items as Todo[]
+
+	for (const item of items) {
+		if (db.todos.read(item.id)) db.todos.update(item.id, item)
+		else db.todos.create(item)
+	}
+
+	const todos = db.todos.list()
+
+	const missingItems = todos.filter(
+		(todo) => !items.find((item) => item.id === todo.id)
+	)
+
+	return c.json({ items: missingItems })
+})
+
+const clients = new Map<string, WSContext>()
+
 app.get(
 	'/ws',
 	upgradeWebSocket((c) => ({
-		onOpen: (evt, ws) => {},
-		onClose: (evt, ws) => {},
-		onMessage: (evt, ws) => {}
+		onOpen: (evt, ws) => {
+			const clientId = c.req.query('clientId') ?? 'unknown'
+			clients.set(clientId, ws)
+			console.log(`Client connected: ${clientId}`)
+		},
+		onClose: (evt, ws) => {
+			const clientId = c.req.query('clientId') ?? 'unknown'
+			clients.delete(clientId)
+			console.log(`Client disconnected: ${clientId}`)
+		},
+		onMessage: (evt, ws) => {
+			const clientId = c.req.query('clientId') ?? 'unknown'
+			const message = JSON.parse(evt.data.toString())
+
+			switch (message.event) {
+				case 'item:put': {
+					const todo = message.todo
+
+					if (db.todos.read(todo.id)) db.todos.update(todo.id, todo)
+					else db.todos.create(todo)
+
+					for (const [, client] of clients) {
+						client.send(
+							JSON.stringify({
+								event: 'item:put',
+								todo
+							})
+						)
+					}
+
+					console.log(`Client ${clientId} put todo: ${todo.id}`)
+					break
+				}
+				case 'item:delete': {
+					const todo = message.todo
+
+					if (db.todos.read(todo.id)) db.todos.delete(todo.id)
+
+					for (const [, client] of clients) {
+						client.send(
+							JSON.stringify({
+								event: 'item:delete',
+								todo
+							})
+						)
+					}
+
+					console.log(`Client ${clientId} delete todo: ${todo.id}`)
+					break
+				}
+				default: {
+					console.log(`Client ${clientId} sent unknown event: ${message.event}`)
+					break
+				}
+			}
+		}
 	}))
 )
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
+import Dexie, { type EntityTable } from 'dexie'
+import { useLiveQuery } from 'dexie-react-hooks'
 import { nanoid } from 'nanoid'
-import { useState } from 'react'
+import { useEffect } from 'react'
 
 type Todo = {
 	id: string
@@ -12,33 +14,122 @@ type Todo = {
 
 const clientId = nanoid()
 
-function App() {
-	const [todos, setTodos] = useState<Todo[]>([])
+const db = new Dexie('todos') as Dexie & {
+	todos: EntityTable<Todo, 'id'>
+}
 
-	const addTodo = (content: string) => {
-		const todo = {
-			id: nanoid(),
-			content,
-			done: false,
-			createdAt: new Date(),
-			version: todos.length,
-			clientId
+db.version(1).stores({
+	todos: 'id, clientId, version'
+})
+
+const ws = new WebSocket(`ws://localhost:3000/ws?clientId=${clientId}`)
+
+ws.addEventListener('open', () => {
+	console.log('Connected to server')
+
+	window.addEventListener('item:put', (e) => {
+		const todo = (e as CustomEvent<{ todo: Todo }>).detail.todo
+
+		ws.send(
+			JSON.stringify({
+				event: 'item:put',
+				todo
+			})
+		)
+	})
+
+	window.addEventListener('item:delete', (e) => {
+		const todo = (e as CustomEvent<{ todo: Todo }>).detail.todo
+
+		ws.send(
+			JSON.stringify({
+				event: 'item:delete',
+				todo
+			})
+		)
+	})
+
+	ws.addEventListener('message', async (e) => {
+		const message = JSON.parse(e.data)
+
+		switch (message.event) {
+			case 'item:put': {
+				const todo = message.todo
+
+				if (await db.todos.get(todo.id)) db.todos.put(todo)
+				else db.todos.add(todo)
+
+				console.log(`Server put todo: ${todo.id}`)
+				break
+			}
+			case 'item:delete': {
+				const todo = message.todo
+
+				if (await db.todos.get(todo.id)) db.todos.delete(todo.id)
+
+				console.log(`Server delete todo: ${todo.id}`)
+				break
+			}
+			default: {
+				console.log(`Server sent unknown event: ${message.event}`)
+				break
+			}
 		}
+	})
+})
 
-		setTodos([...todos, todo])
-	}
+function App() {
+	const todos = useLiveQuery(() => db.todos.toCollection().sortBy('createdAt'))
 
-	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+	const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 
 		const formData = new FormData(e.target as HTMLFormElement)
 
 		const content = formData.get('content') as string
 
-		addTodo(content)
+		const todo: Todo = {
+			id: nanoid(),
+			content,
+			done: false,
+			createdAt: new Date(),
+			version: todos?.length ?? 1,
+			clientId
+		}
 
 		e.currentTarget.reset()
+
+		await db.todos.add(todo).then(() => {
+			window.dispatchEvent(
+				new CustomEvent('item:put', {
+					detail: { todo }
+				})
+			)
+		})
 	}
+
+	useEffect(() => {
+		async function sync() {
+			const response = await fetch('http://localhost:3000/sync', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					items: await db.todos.toArray()
+				})
+			})
+
+			const { items } = await response.json()
+
+			for (const todo of items) {
+				if (await db.todos.get(todo.id)) db.todos.put(todo)
+				else db.todos.add(todo)
+			}
+		}
+
+		sync()
+	}, [])
 
 	return (
 		<>
@@ -48,10 +139,37 @@ function App() {
 			</form>
 
 			<ul>
-				{todos.map((todo) => (
+				{todos?.map((todo) => (
 					<li key={todo.id}>
-						<input type="checkbox" checked={todo.done} />
+						<input
+							type="checkbox"
+							checked={todo.done}
+							onChange={async (e) => {
+								await db.todos.update(todo.id, { done: e.target.checked })
+
+								window.dispatchEvent(
+									new CustomEvent('item:put', {
+										detail: { todo }
+									})
+								)
+							}}
+						/>
 						{todo.content}
+
+						<button
+							type="button"
+							onClick={async () => {
+								await db.todos.delete(todo.id)
+
+								window.dispatchEvent(
+									new CustomEvent('item:delete', {
+										detail: { todo }
+									})
+								)
+							}}
+						>
+							Delete
+						</button>
 					</li>
 				))}
 			</ul>
